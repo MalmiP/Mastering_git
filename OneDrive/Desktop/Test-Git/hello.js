03e5b219af1050ba3846a84cc63b1e70b8fb28c1
Malmi <malmi.20222148@iit.ac.lk>
2024-10-13 19:09:48 +0530

    add savee

--- OneDrive/Desktop/Test-Git/hello.js
+++ OneDrive/Desktop/Test-Git/hello.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-console.log('Hello, git!');
-console.log('Hello, github!');
-console.log('Hello, world!');
-console.log('Helloooooo!');
-console.log('revert');
-=======
-console.log('Hello, git!');
->>>>>>> parent of c32f8e1 (add github console.log into hello.js)
+
+console.log('Hello, gittttttttttt!');
