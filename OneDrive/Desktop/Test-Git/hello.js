d61723b119281bcceb407c338172c96e3b904123
Malmi <malmi.20222148@iit.ac.lk>
2024-10-13 19:01:16 +0530

    Revert "add github console.log into hello.js"
    This reverts commit c32f8e1e8b4f2d9e5ddda7ed335744b99c205fa8.

--- OneDrive/Desktop/Test-Git/hello.js
+++ OneDrive/Desktop/Test-Git/hello.js
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 console.log('Hello, git!');
 console.log('Hello, github!');
 console.log('Hello, world!');
 console.log('Helloooooo!');
 console.log('revert');
+=======
+console.log('Hello, git!');
+>>>>>>> parent of c32f8e1 (add github console.log into hello.js)
